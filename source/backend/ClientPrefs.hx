47adde83864fa63c6e7c83dff6c0c2f7b6132be2
Santiago <calebesantiago206@gmail.com>
2025-04-03 13:32:33 -0300

    ClientPrefs

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -25,6 +25,7 @@ import states.TitleState;
 	public var camZooms:Bool = true;
 	public var hideHud:Bool = false;
 	public var windowDarkMode:Bool = false;
+	public var backdrop:Bool = true;
 	public var noteOffset:Int = 0;
 	public var arrowRGB:Array<Array<FlxColor>> = [
 		[0xFFC24B99, 0xFFFFFFFF, 0xFF3C1F56],
