a0d167ad2e8728a0c695a76ac8cbef770ca11594
Ender69 <112335251+ender69flock@users.noreply.github.com>
2026-04-30 12:36:59 +0500

    health icon cleanup <3

--- source/objects/HealthIcon.hx
+++ source/objects/HealthIcon.hx
@@ -22,9 +22,7 @@ class HealthIcon extends FlxSprite
 	public function new(char:String = 'face', isPlayer:Bool = false, ?allowGPU:Bool = true, ?offsetsThing:Array<Float>, ?scale:Float = 1, ?flipX:Bool = false, ?blendThing:String = '', ?fpsThing:Int = 24)
 	{
 		super();
-
 		if (offsetsThing == null) offsetsThing = [0, 0];
-
 		this.isPlayer = isPlayer;
 		changeIcon(char, allowGPU, offsetsThing, scale, flipX, blendThing, fpsThing);
 		scrollFactor.set();
@@ -33,46 +31,34 @@ class HealthIcon extends FlxSprite
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
-
-		if (sprTracker != null)
-			setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
+		if (sprTracker != null) setPosition(sprTracker.x + sprTracker.width + 12, sprTracker.y - 30);
 	}
 
 	public function changePar(?iconOffsetsPar:Array<Float>, ?scalePar:Float = 1, ?flipXpar:Bool = false, ?blendThing:String = '', ?fpsThing:Int = 24)
 	{
 		if (iconOffsetsPar == null) iconOffsetsPar = [0, 0];
-
 		iconOffsets[0] = (width - 150) + iconOffsetsPar[0];
 		iconOffsets[1] = (height - 150) + iconOffsetsPar[1];
-
 		scale.set(scalePar, scalePar);
 		flipX = flipXpar;
 		updateHitbox();
-
 		animation.curAnim.frameRate = Std.int(fpsThing);
-
 		blend = LuaUtils.blendModeFromString(blendThing);
 	}
 
 	public function changeIcon(char:String, ?allowGPU:Bool = true, ?iconOffsetsPar:Array<Float>, ?scalePar:Float = 1, ?flipXpar:Bool = false, ?blendThing:String = '', ?fpsThing:Int = 24) {
-		if(this.char != char) {
+		if(this.char != char)
+		{
 			if (iconOffsetsPar == null) iconOffsetsPar = [0, 0];
-
 			var name:String = 'icons/' + char;
-			
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-face'; //Prevents crash from missing icon
-
 			var animToFind:String = Paths.getPath('images/' + name + '.xml', TEXT);
-
 			if (#if MODS_ALLOWED FileSystem.exists(animToFind) || #end Assets.exists(animToFind))
 			{
 				animated = true;
-
 				var graphic = Paths.getSparrowAtlas(name, allowGPU);
-
 				frames = graphic;
-
 				animation.addByPrefix('idle', 'idle', fpsThing, true, isPlayer);
 				animation.addByPrefix('winning', 'winning', fpsThing, true, isPlayer);
 				animation.addByPrefix('losing', 'losing', fpsThing, true, isPlayer);
@@ -81,48 +67,24 @@ class HealthIcon extends FlxSprite
 				animation.addByPrefix('fromWinning', 'fromWinning', fpsThing, false, isPlayer);
 				animation.addByPrefix('fromLosing', 'fromLosing', fpsThing, false, isPlayer);
 				animation.play('idle');
-
-				iconOffsets[0] = (width - 150) + iconOffsetsPar[0];
-				iconOffsets[1] = (height - 150) + iconOffsetsPar[1];
-				scale.set(scalePar, scalePar);
-				flipX = flipXpar;
-				updateHitbox();
-
-				blend = LuaUtils.blendModeFromString(blendThing);
-				
-				this.char = char;
-
-				if(char.endsWith('-pixel'))
-					antialiasing = false;
-				else
-					antialiasing = ClientPrefs.data.antialiasing;
 			}
 			else
 			{
 				var graphic = Paths.image(name, allowGPU);
 				var iSize:Float = Math.round(graphic.width / graphic.height);
-				loadGraphic(graphic, true, Math.floor(graphic.width / iSize), Math.floor(graphic.height));
-				iconOffsets[0] = (width - 150) / iSize + iconOffsetsPar[0];
-				iconOffsets[1] = (height - 150) / iSize + iconOffsetsPar[1];
-				scale.set(scalePar, scalePar);
-				flipX = flipXpar;
-				updateHitbox();
-	
+				loadGraphic(graphic, true, Math.floor(graphic.width / iSize), Math.floor(graphic.height));	
 				animation.add(char, [for(i in 0...frames.frames.length) i], 0, false, isPlayer);
 				animation.play(char);
-	
-				if (animation.curAnim.numFrames == 3)
-					hasThirdIcon = true;
-
-				blend = LuaUtils.blendModeFromString(blendThing);
-	
-				this.char = char;
-	
-				if(char.endsWith('-pixel'))
-					antialiasing = false;
-				else
-					antialiasing = ClientPrefs.data.antialiasing;
+				if (animation.curAnim.numFrames == 3) hasThirdIcon = true;
 			}
+			iconOffsets[0] = (width - 150) + iconOffsetsPar[0];
+			iconOffsets[1] = (height - 150) + iconOffsetsPar[1];
+			scale.set(scalePar, scalePar);
+			flipX = flipXpar;
+			updateHitbox();
+			blend = LuaUtils.blendModeFromString(blendThing);
+			this.char = char;
+			antialiasing = char.endsWith('-pixel') ? false : ClientPrefs.data.antialiasing;
 		}
 	}
 
@@ -135,74 +97,42 @@ class HealthIcon extends FlxSprite
 	public function hasAnimation(id:String):Bool
 	{
 		if (animation == null) return false;
-	  
 		return animation.getByName(id) != null;
 	}
 
-	public function isAnimationFinished():Bool
-	{
-		return this.animation.finished;
-	}
-
+	public function isAnimationFinished():Bool return this.animation.finished;
 	public function updateHealthIcon(health:Float):Void
-	{
-		// We want to efficiently handle animation playback
-	  
-		// Here, we use the current animation name to track the current state
-		// of a simple state machine. Neat!
-	  
+	{ 
 		switch (getCurrentAnimation())
 		{
 			case 'idle':
-				if (health < 20)
-					playAnimation('toLosing', 'losing');
-			  	else if (health > 80)
-					playAnimation('toWinning', 'winning');
-			  	else
-					playAnimation('idle');
-
+				if (health < 20) playAnimation('toLosing', 'losing');
+			  	else if (health > 80) playAnimation('toWinning', 'winning');
+			  	else playAnimation('idle');
 			case 'winning':
 				if (health < 80)
 					playAnimation('fromWinning', 'idle');
 			  	else
 					playAnimation('winning', 'idle');
-
 			case 'losing':
-			  	if (health > 20) 
-					playAnimation('fromLosing', 'idle');
-			  	else
-					playAnimation('losing', 'idle');
-
-			case 'toLosing':
-			  	if (isAnimationFinished())
-					playAnimation('losing', 'idle');
-
-			case 'toWinning':
-			  	if (isAnimationFinished())
-					playAnimation('winning', 'idle');
-
-			case 'fromLosing' | 'fromWinning':
-			  	if (isAnimationFinished())
-					playAnimation('idle');
-
-			case '':
-			  	playAnimation('idle');
-
-			default:
-			  	playAnimation('idle');
+			  	if (health > 20) playAnimation('fromLosing', 'idle');
+			  	else playAnimation('losing', 'idle');
+			case 'toLosing': if (isAnimationFinished()) playAnimation('losing', 'idle');
+			case 'toWinning': if (isAnimationFinished()) playAnimation('winning', 'idle');
+			case 'fromLosing' | 'fromWinning': if (isAnimationFinished()) playAnimation('idle');
+			case '': playAnimation('idle');
+			default: playAnimation('idle');
 		}
 	}
 
 	public function playAnimation(name:String, fallback:String = null, restart = false):Void
 	{
-		// Attempt to play the animation
 		if (hasAnimation(name))
 		{
 			animation.play(name, restart, false, 0);
 			return;
 		}
-	  
-		// Play the fallback animation if the requested animation was not found
+
 		if (fallback != null && hasAnimation(fallback))
 		{
 			animation.play(fallback, restart, false, 0);
@@ -221,7 +151,5 @@ class HealthIcon extends FlxSprite
 		}
 	}
 
-	public function getCharacter():String {
-		return char;
-	}
+	public function getCharacter():String return char;
 }
